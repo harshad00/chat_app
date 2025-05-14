8fbe021578c70c55844369e42b300fdb2acc7cc8
Harshad Prajapati <bholo0090@gmail.com>
2025-05-14 19:21:17 +0530

    aad male uderstnde

--- utils/sendVerificationEmail.js
+++ utils/sendVerificationEmail.js
@@ -7,7 +7,7 @@ const sendVerificationEmail = async (email, token) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: false, // true for 465, false for 587
+    secure: true, // true for 465, false for 587
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -17,10 +17,10 @@ const sendVerificationEmail = async (email, token) => {
   const verificationUrl = `https://chat-app-zrf6.onrender.com/api/verify-email?token=${token}`;
 
   const mailOptions = {
-    from: `"Your App Name" <${process.env.EMAIL_USER}>`,
+    from: `"My Chat App" <${process.env.EMAIL_USER}>`,
     to: email,
     subject: 'Verify Your Email',
-    html: `<p>Click the link to verify your email: <a href="${verificationUrl}">Verify</a></p>`,
+    html: `<p>Click the link to verify your email: <a href="${verificationUrl}">Verify</a> <br/> <i> Harshad Prajapati</i> </p>`,
   };
 
   try {
